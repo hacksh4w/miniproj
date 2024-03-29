837087fb3c2c6ecd7ca8f466fdb548b3375124e2
hacksh4w <mrinalsnair@gmail.com>
2024-03-29 16:57:24 +0530

    add folder structure

---
+++ contexts/useTheme.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function useTheme(props) {
+  return (
+    <div>useTheme</div>
+  )
+}
+
+useTheme.propTypes = {}
+
+export default useTheme
---
+++ hooks/useHook.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function useHook() {
+  return (
+    <div>useHook</div>
+  )
+}
+
+export default useHook
