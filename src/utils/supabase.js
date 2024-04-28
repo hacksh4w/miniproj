5376adaa602111e119830617ce957bb81dccd0be
hacksh4w <mrinalsnair@gmail.com>
2024-04-28 23:05:34 +0530

    fix imports

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -4,6 +4,6 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl) throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_URL');
-if (!supabaseKey) throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_KEY');
+if (!supabaseKey) throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_ANON_KEY');
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
