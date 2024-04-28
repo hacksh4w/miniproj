796f12623466349f5c1509a76c997acfa478f4a8
hacksh4w <mrinalsnair@gmail.com>
2024-04-28 22:17:10 +0530

    private page logic added

--- src/app/private/page.jsx
+++ src/app/private/page.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { redirect } from 'next/navigation'
+import { createClient } from '@/utils/supabase/server'
 
-function page() {
-  return (
-    <div>page</div>
-  )
+export default async function PrivatePage() {
+  const supabase = createClient()
+
+  const { data, error } = await supabase.auth.getUser()
+  if (error || !data?.user) {
+    redirect('/login')
+  }
+
+  return <p>Hello {data.user.email}</p>
 }
 
-export default page
+//ithoru private page, that can be accessed only by the user thats logged in and only for their session
