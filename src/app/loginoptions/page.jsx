bc14aa36ed48d534ead59c0e31d6222bc637877b
hacksh4w <mrinalsnair@gmail.com>
2024-04-29 22:54:50 +0530

    feat : added carousel arrows

--- src/app/loginoptions/page.jsx
+++ src/app/loginoptions/page.jsx
@@ -7,14 +7,14 @@ export default function loginoptions() {
   return (
     <Container maxW='100vw' bg='white' >
       <Flex  minH='100vh' maxW='95%' direction='column' justify="center" align="center" mx={8}>
-        <Stack spacing={8} direction='column' align='center'>
-        <Link href="/login">
-          <Button colorScheme='teal' variant='solid' size='lg' fontWeight={500}>
+        <Stack spacing={12} color='grey' direction='column' align='center' orderWidth='1px' borderColor='blue'>
+          <Link href="/login">
+            <Button colorScheme='orange' variant='solid' size='lg' fontWeight={600}>
               Sign in for Customers
             </Button>
             </Link>
             <Link href="/shoplogin">
-            <Button colorScheme='teal' variant='solid' size='lg'  fontWeight={500}>
+            <Button colorScheme='teal' variant='solid' size='lg'  fontWeight={600}>
               Sign in for Shops & Shop Owners
             </Button>
             </Link>
@@ -22,7 +22,7 @@ export default function loginoptions() {
             <Button colorScheme='teal' variant='solid' size='lg'fontWeight={500}>
               Sign in for Admin
             </Button>
-            </Link>
+           </Link>
         </Stack>
       </Flex>
     </Container>
