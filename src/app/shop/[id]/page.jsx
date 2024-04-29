6dcb111cddd966f25046cce26cc70cd293864e8d
Alaka A J <alaka03aj@gmail.com>
2024-04-30 00:24:35 +0530

    shop page w db

--- src/app/shop/[id]/page.jsx
+++ src/app/shop/[id]/page.jsx
@@ -1,58 +1,138 @@
-// Logic: Each shop has an ID. On clicking the id, id is passed over here and fetches data from db and displays it over here.
-
+"use client";
 import { Navbar } from "../../../components";
-import Image from "next/image";
+import {
+  Box,
+  Container,
+  Flex,
+  Heading,
+  Image,
+  SimpleGrid,
+  Text,
+  Tag
+} from "@chakra-ui/react";
+import { supabase } from "../../../utils/supabase";
+import { useState, useEffect } from "react";
 
 const shop = ({ params }) => {
-  // logic for fetching data from db
+  const [shopData, setShopData] = useState(null);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const fetchShopData = async () => {
+      try {
+        const { data, error } = await supabase
+          .from("shops")
+          .select("*")
+          .eq("id", params.id)
+          .single();
+
+        if (error) {
+          throw error;
+        }
+        setShopData(data);
+      } catch (error) {
+        console.error("Error fetching shop data:", error.message);
+      }
+    };
+
+    const fetchItems = async () => {
+      try {
+        const { data: items, error } = await supabase
+          .from("items")
+          .select("*")
+          .eq("shop_id", params.id);
+
+        if (error) {
+          throw error;
+        }
+
+        setItems(items);
+      } catch (error) {
+        console.error("Error fetching items:", error.message);
+      }
+    };
+
+    fetchShopData();
+    fetchItems();
+  }, [params.id]);
+
   return (
     <>
       <Navbar />
-      <div class="w-screen flex-col gap-10 sm:px-16 px-6 flex justify-center items-center lg:pt-16 lg:pb-[140px] md:pt-16 md:pb-[140px] sm:pt-16 sm:pb-[140px] ss:pt-16 ss:pb-[140px] xs:pt-4 xs:pb-[140px] pt-4 pb-[140px] bg-gradient">
-        <div class="bg-white w-10/12 gap-10 lg:max-w-screen-lg lg:flex-row lg:gap-40 md:max-w-screen-md md:flex-row md:gap-40 sm:max-w-screen-sm ss:max-w-screen-xs xs:max-w-screen-xs p-10 rounded-lg shadow-lg flex flex-col items-center justify-start">
-          <div class="flex-none w-1/2 sm:w-1/3">
-            <Image
-              src="https://upload.wikimedia.org/wikipedia/commons/5/52/Free_logo.svg"
-              alt="Your Image"
-              width={24}
-              height={24}
-              class="w-full h-auto rounded-lg"
-            />
-          </div>
-          <div class="flex-grow pl-4">
-            <h2 class="text-lg font-semibold mb-2">Shop Name</h2>
-            <p>Rating: /5</p>
-            <p class="text-sm">Desc</p>
-            <p>Location</p>
-            <p>Phone</p>
-          </div>
-        </div>
+      <Container
+        maxW="full"
+        px={{ base: "6", sm: "16" }}
+        py={{ base: "4", sm: "16", lg: "16" }}
+        display="flex"
+        flexDir="column"
+        justifyContent="center"
+        alignItems="center"
+        bgGradient="linear(to-t, yellow.400, orange.400)"
+      >
+        {shopData && (
+          <Flex
+            minW="80%"
+            bg="white"
+            rounded="lg"
+            boxShadow="xl"
+            p="5"
+            flexDir={{ base: "column", md: "row", lg: "row" }}
+            alignItems="center"
+            justifyContent="space-around"
+          >
+            <Box flex="row" w="1/3">
+              <Image
+                src={shopData.shopimg}
+                alt="Shop Image"
+                boxSize={{ base: "200px", md: "300px" }}
+                objectFit="contain"
+                rounded="lg"
+              />
+            </Box>
+            <Box flex="row" gap={{ base: "0", md: "20" }}>
+              <Heading as="h2" size="lg" mb="2">
+                {shopData.name}
+              </Heading>
+              <Box>
+                <p>Rating: {shopData.shoprate}/5</p>
+                <p>{shopData.address}</p>
+                <p>
+                  {shopData.city}, {shopData.state}
+                </p>
+                <p>{shopData.pincode}</p>
+                <p>
+                  Location: {shopData.latitude}, {shopData.longitude}
+                </p>
+                <p>Owned By: {shopData.owner_name}</p>
+                <p>Phone: {shopData.sphone}</p>
+              </Box>
+            </Box>
+          </Flex>
+        )}
 
-        <div class="bg-white w-10/12 gap-10 flex-col lg:max-w-screen-lg flex-rowmd:max-w-screen-md sm:max-w-screen-sm ss:max-w-screen-xs xs:max-w-screen-xs p-10 rounded-lg shadow-lg flex flex-row items-center justify-start">
-          <h2>Available Items</h2>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-            <div className="px-4 border border-orange-500 rounded-xl max-w-[400]">
-              <div>
-                <Image
-                  className="w-full h-auto cursor-pointer"
-                  src="https://upload.wikimedia.org/wikipedia/commons/5/52/Free_logo.svg"
-                  width={200}
-                  height={300}
-                  alt="img"
-                />
-              </div>
-              <div className="space-y-2 py-2">
-                <h2 className="text-orange-400 uppercase text-center font-bold">
-                  Free
-                </h2>
-                <p className="text-gray-500 max-w-[150px]">Totally free</p>
-                <p>Rating</p>
-                <div className="font-bold flex gap-4">Rs. 1000</div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+<br />    
+<Heading size={{base: "sm", md:"md"}} mb='10px' >Available Products</Heading>
+        <SimpleGrid columns={{ base: 1, md: 4 }} spacing="8">
+          {items.map((item, index) => (
+            <Box key={index} p="4" borderWidth="1px" borderRadius="lg" bg="white">
+              <Image
+                src={item.imageUrl}
+                alt="Product Image"
+                boxSize={{ base: "250px", md: "300px" }}
+                objectFit="cover"
+                rounded="lg"
+              />
+              <br />
+              <Text fontWeight="bold">{item.name}</Text>               
+              <Tag colorScheme="yellow">{item.category}</Tag>
+
+              <Text>{item.description}</Text>
+              <p>Rating: {item.rating}/5</p>
+              <Text>Price: Rs. {item.price}</Text>
+            </Box>
+          ))}
+        </SimpleGrid>
+      </Container>
     </>
   );
 };
